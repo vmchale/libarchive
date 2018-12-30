cb8e3840be2df4013f60eb4143afa18828227d89
Vanessa McHale <vanessa.mchale@iohk.io>
2018-12-30 01:28:47 -0600

    stylish-haskell

--- src/Codec/Archive.hs
+++ src/Codec/Archive.hs
@@ -3,9 +3,9 @@ module Codec.Archive
     , unpackTarball
     ) where
 
-import Codec.Archive.Foreign (unpackToDir)
-import qualified Data.ByteString as BS
-import System.FilePath (pathSeparator)
+import           Codec.Archive.Foreign (unpackToDir)
+import qualified Data.ByteString       as BS
+import           System.FilePath       (pathSeparator)
 
 unpackTarball :: FilePath -- ^ Filepath pointing to @.tar@ file
               -> FilePath -- ^ Filepath to unpack to
--- src/Codec/Archive/Foreign.hs
+++ src/Codec/Archive/Foreign.hs
@@ -1,18 +1,18 @@
 module Codec.Archive.Foreign ( unpackToDir
                              ) where
-import Foreign.Ptr
-import Foreign.C.String
-import Foreign.C.Types
-import Data.ByteString as BS
-import Data.Word (Word)
+import           Data.ByteString  as BS
+import           Data.Word        (Word)
+import           Foreign.C.String
+import           Foreign.C.Types
+import           Foreign.Ptr
 
 foreign import ccall unsafe unpack_in_dir :: CString -> Ptr CChar -> Word -> IO ()
 
-unpackToDir :: FilePath 
+unpackToDir :: FilePath
             -> BS.ByteString
             -> IO ()
 unpackToDir fp bs = do
     fp' <- newCString fp
     useAsCStringLen bs $
-        \(charPtr, sz) -> 
+        \(charPtr, sz) ->
             unpack_in_dir fp' charPtr (fromIntegral sz)
