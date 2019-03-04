f727db2c12056e294715c9bdfe82cde7730c41ba
Vanessa McHale <vanessa.mchale@iohk.io>
2019-03-03 19:09:33 -0600

    Use different architecture

--- src/Codec/Archive.hs
+++ src/Codec/Archive.hs
@@ -12,8 +12,6 @@ import           Foreign.C.String
 import           Foreign.Marshal.Alloc (alloca)
 import           Foreign.Ptr           (Ptr)
 import           Foreign.Storable      (Storable (..))
-import           System.Directory      (createDirectoryIfMissing,
-                                        withCurrentDirectory)
 
 done :: ReadResult -> Bool
 done res = not (res == archiveOk || res == archiveRetry)
@@ -42,9 +40,8 @@ getEntry a = alloca $ \ptr -> do
 unpackArchive :: FilePath -- ^ Filepath pointing to archive
               -> FilePath -- ^ Filepath to unpack to
               -> IO ()
-unpackArchive tarFp dirFp =
-    withMissingDir dirFp $
-        unpackArchive' =<< archiveFile tarFp
+unpackArchive tarFp _dirFp =
+    unpackArchive' =<< archiveFile tarFp
 
 bsToArchive :: BS.ByteString -> IO (Ptr Archive)
 bsToArchive bs = do
@@ -55,14 +52,8 @@ bsToArchive bs = do
             archive_read_open_memory a charPtr (fromIntegral sz)
     pure a
 
-withMissingDir :: FilePath -> IO a -> IO a
-withMissingDir fp act = do
-    createDirectoryIfMissing True fp
-    withCurrentDirectory fp act
-
 unpackToDir :: FilePath -- ^ Directory to unpack in
             -> BS.ByteString -- ^ 'ByteString' containing archive
             -> IO ()
-unpackToDir fp bs =
-    withMissingDir fp $
-        unpackArchive' =<< bsToArchive bs
+unpackToDir _fp bs =
+    unpackArchive' =<< bsToArchive bs
--- src/Codec/Archive/Foreign.chs
+++ src/Codec/Archive/Foreign.chs
@@ -1,8 +1,5 @@
-module Codec.Archive.Foreign ( -- * cbits
-                               unpack_in_dir
-                             , unpack_from_file
-                             -- * Direct bindings
-                             , archive_read_new
+module Codec.Archive.Foreign ( -- * Direct bindings
+                               archive_read_new
                              , archive_read_support_format_all
                              , archive_entry_set_pathname
                              , archive_read_data_skip
@@ -36,9 +33,6 @@ import           Data.Word           (Word)
 import           Foreign.C.String
 import           Foreign.C.Types
 import           Foreign.Ptr
-
-foreign import ccall unsafe unpack_in_dir :: CString -> Ptr CChar -> Word -> IO ()
-foreign import ccall unsafe unpack_from_file :: CString -> CString -> IO ()
 
 foreign import ccall unsafe archive_read_new :: IO (Ptr Archive)
 foreign import ccall unsafe archive_read_support_format_all :: Ptr Archive -> IO ()
