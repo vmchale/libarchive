53ea66fb86bd40321139b1c9b54434ba64ef445d
Vanessa McHale <vanessa.mchale@iohk.io>
2019-03-22 19:52:31 -0500

    Documentation improved

--- src/Codec/Archive/Foreign.hs
+++ src/Codec/Archive/Foreign.hs
@@ -1,9 +1,10 @@
 -- | Everything here is stateful and hence takes place in the 'IO' monad.
 --
--- Consult @archive.h@ or @archive_entry.h@ for documentation.
+-- Consult @archive.h@ or @archive_entry.h@ for documentation. Functions that
+-- are deprecated in the C API are not exposed here at all.
 module Codec.Archive.Foreign ( module Codec.Archive.Foreign.ArchiveEntry
                              , module Codec.Archive.Foreign.Archive
                              ) where
 
-import Codec.Archive.Foreign.ArchiveEntry
-import Codec.Archive.Foreign.Archive
+import           Codec.Archive.Foreign.Archive
+import           Codec.Archive.Foreign.ArchiveEntry
