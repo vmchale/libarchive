d9c636cfea74eb17db6734c1ef295da65a0b4c59
Vanessa McHale <vanessa.mchale@iohk.io>
2019-03-27 12:37:45 -0500

    More progress; making this sensible

--- src/Codec/Archive/Common.hs
+++ src/Codec/Archive/Common.hs
@@ -1,8 +1,14 @@
-module Codec.Archive.Common ( actFree ) where
+module Codec.Archive.Common ( actFree
+                            , actFreeCallback
+                            ) where
 
 import           Codec.Archive.Foreign
+import           Foreign.Marshal.Alloc (free)
 import           Foreign.Ptr
 
 -- | Read from an 'Archive' and then free it
 actFree :: (Ptr Archive -> IO a) -> Ptr Archive -> IO a
 actFree fact a = fact a <* archive_free a
+
+actFreeCallback :: (Ptr Archive -> IO a) -> (Ptr Archive, FunPtr (ArchiveCloseCallback b), Ptr c) -> IO a
+actFreeCallback fact (a, cc, bufPtr) = fact a <* archive_free a <* freeHaskellFunPtr cc <* free bufPtr
--- src/Codec/Archive/Unpack/Lazy.hs
+++ src/Codec/Archive/Unpack/Lazy.hs
@@ -12,53 +12,56 @@ import           Data.ByteString       (useAsCStringLen)
 import qualified Data.ByteString.Lazy  as BSL
 import           Data.Functor          (($>))
 import           Data.IORef            (modifyIORef, newIORef, readIORef)
+import           Foreign.C.Types
 import           Foreign.Marshal.Alloc (free, mallocBytes)
 import           Foreign.Ptr
 import           Foreign.Storable      (poke)
 import           System.IO.Unsafe      (unsafePerformIO)
 
+foreign import ccall memcpy :: Ptr a -- ^ Destination
+                            -> Ptr b -- ^ Source
+                            -> CSize -- ^ Size
+                            -> IO (Ptr a) -- ^ Pointer to destination
+
 unpackToDirLazy :: FilePath -- ^ Directory to unpack in
                 -> BSL.ByteString -- ^ 'BSL.ByteString' containing archive
                 -> IO ()
 unpackToDirLazy fp bs = do
-    a <- bslToArchive bs
+    (a, cc, bufPtr) <- bslToArchive bs
     unpackEntriesFp a fp
     void $ archive_read_free a
+    void $ freeHaskellFunPtr cc
+    void $ free bufPtr
 
 -- | Read an archive lazily. The format of the archive is automatically
 -- detected.
 readArchiveBSL :: BSL.ByteString -> [Entry]
-readArchiveBSL = unsafePerformIO . (actFree hsEntries <=< bslToArchive)
+readArchiveBSL = unsafePerformIO . (actFreeCallback hsEntries <=< bslToArchive)
 
 -- | Lazily stream a 'BSL.ByteString'
-bslToArchive :: BSL.ByteString -> IO (Ptr Archive)
+bslToArchive :: BSL.ByteString -> IO (Ptr Archive, FunPtr (ArchiveCloseCallback a), Ptr a)
 bslToArchive bs = do
     a <- archive_read_new
     void $ archive_read_support_format_all a
+    bufPtr <- mallocBytes (32 * 1024) -- default to 32k byte chunks; should really do something more rigorous
     bsChunksRef <- newIORef bsChunks
-    rc <- mkReadCallback (readBSL bsChunksRef)
+    rc <- mkReadCallback (readBSL bsChunksRef bufPtr)
     cc <- mkCloseCallback (\_ ptr -> freeHaskellFunPtr rc *> free ptr $> archiveOk)
     nothingPtr <- mallocBytes 0
     sequence_ [ archive_read_set_read_callback a rc
               , archive_read_set_close_callback a cc
               , archive_read_set_callback_data a nothingPtr
-              -- Sooo readBSL bsChunksRef a nothingPtr nothingPtr works BUT when
-              -- I use the wrapper it's all fucked??
-              --
-              -- I should try to pin down a minimal case...
-              --
-              -- this is where it hangs indefinitely
               , archive_read_open1 a
               ]
-    pure a
-    where readBSL bsRef _ _ dataPtr = do
-                -- when readBSL is called it's fucked??
+    pure (a, cc, bufPtr)
+    where readBSL bsRef bufPtr _ _ dataPtr = do
                 bs' <- readIORef bsRef
                 case bs' of
                     [] -> pure 0
                     (x:_) -> do
                         modifyIORef bsRef tail
-                        useAsCStringLen x $ \(charPtr, sz) ->
-                            poke dataPtr charPtr $>
-                            fromIntegral sz
+                        useAsCStringLen x $ \(charPtr, sz) -> do
+                            -- FIXME: should be freed..? foreignPtr?
+                            void $ memcpy bufPtr charPtr (fromIntegral sz)
+                            poke dataPtr bufPtr $> fromIntegral sz
           bsChunks = BSL.toChunks bs
