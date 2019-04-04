871f1e21890bff1ce51eab79a7f9dcada60ff4b4
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-04 12:01:38 -0500

    Update docs

--- src/Codec/Archive/Common.hs
+++ src/Codec/Archive/Common.hs
@@ -5,9 +5,11 @@ module Codec.Archive.Common ( actFree
 import           Codec.Archive.Foreign
 import           Foreign.Ptr
 
--- | Read from an 'Archive' and then free it
+-- | Do something with an 'Archive' and then free it
 actFree :: (Ptr Archive -> IO a) -> Ptr Archive -> IO a
 actFree fact a = fact a <* archive_free a
 
-actFreeCallback :: (Ptr Archive -> IO a) -> (Ptr Archive, IO ()) -> IO a
+actFreeCallback :: (Ptr Archive -> IO a)
+                -> (Ptr Archive, IO ()) -- ^ 'Ptr' to an 'Archive' and an 'IO' action to clean up when done
+                -> IO a
 actFreeCallback fact (a, freeAct) = fact a <* archive_free a <* freeAct
