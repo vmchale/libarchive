4a49ad112262528f2fbe65d61b0a8d4084b968f7
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-13 14:22:35 -0500

    Add some more flattering benchmarks :p

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
 import           Codec.Archive
-import           Codec.Archive.Tar    (Entries (..), FormatError)
-import qualified Codec.Archive.Tar    as Tar
+import           Codec.Archive.Tar      (Entries (..), FormatError)
+import qualified Codec.Archive.Tar      as Tar
+import           Control.Monad.IO.Class (liftIO)
 import           Criterion.Main
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy   as BSL
+import           System.IO.Temp         (withSystemTempDirectory)
 
 roundtrip :: BSL.ByteString -> Either ArchiveResult BSL.ByteString
 roundtrip = fmap entriesToBSL . readArchiveBSL
@@ -17,12 +19,24 @@ failTar (Fail e)    = Left e
 roundtripTar :: BSL.ByteString -> Either FormatError BSL.ByteString
 roundtripTar = fmap Tar.write . failTar . Tar.read
 
+unpack :: IO (Either ArchiveResult ())
+unpack = withSystemTempDirectory "libarchive" $
+    \fp -> runArchiveM $ unpackToDirLazy fp =<< liftIO (BSL.readFile "libarchive-0.2.1.2.tar")
+
+extractTar :: IO ()
+extractTar = withSystemTempDirectory "tar" $
+    \fp -> Tar.extract fp "libarchive-0.2.1.2.tar"
+
 main :: IO ()
 main =
     defaultMain [ env file $ \ f ->
                   bgroup "roundtrip"
                       [ bench "libarchive" $ nf roundtrip f
                       , bench "tar" $ nf roundtripTar f
+                      ]
+                , bgroup "unpack"
+                      [ bench "libarchive" $ nfIO unpack
+                      , bench "tar" $ nfIO extractTar
                       ]
                 ]
     where file = BSL.readFile "libarchive-0.2.1.2.tar"
