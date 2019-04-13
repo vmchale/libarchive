a7e56d9a28e5c49c67bced19286a428896365923
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-12 19:42:36 -0500

    Improve documentation

--- src/Codec/Archive/Permissions.hs
+++ src/Codec/Archive/Permissions.hs
@@ -7,5 +7,6 @@ import           Codec.Archive.Types
 standardPermissions :: Permissions
 standardPermissions = 0o644
 
+-- | Also used for directories
 executablePermissions :: Permissions
 executablePermissions = 0o755
