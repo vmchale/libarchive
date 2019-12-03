cd7c70a9e18e0632f07576757311ce6cd6b04dd5
Vanessa McHale <vamchale@gmail.com>
2019-12-02 21:37:05 -0600

    More efficient

--- src/Codec/Archive/Pack/Common.hs
+++ src/Codec/Archive/Pack/Common.hs
@@ -2,15 +2,14 @@ module Codec.Archive.Pack.Common ( mkEntry ) where
 
 import           Codec.Archive.Types
 import qualified Data.ByteString          as BS
-import           System.PosixCompat.Files (fileGroup, fileMode, fileOwner,
-                                           getFileStatus, isDirectory,
-                                           isRegularFile, isSymbolicLink,
-                                           linkCount, readSymbolicLink)
+import           System.PosixCompat.Files (FileStatus, fileGroup, fileMode, fileOwner, getFileStatus, isDirectory, isRegularFile, isSymbolicLink, linkCount,
+                                           readSymbolicLink)
 
-mkContent :: FilePath -> IO EntryContent
-mkContent fp = do
-    status <- getFileStatus fp
+mkContent :: FilePath -> FileStatus -> IO EntryContent
+mkContent fp status =
     let res = (isRegularFile status, isDirectory status, isSymbolicLink status, linkCount status)
+    in
+
     case res of
         (True, False, False, 1) -> NormalFile <$> BS.readFile fp
         (True, False, False, _) -> pure $ Hardlink fp
@@ -21,6 +20,6 @@ mkContent fp = do
 mkEntry :: FilePath -> IO Entry
 mkEntry fp = do
     status <- getFileStatus fp
-    content' <- mkContent fp
+    content' <- mkContent fp status
     pure $ Entry fp content' (fileMode status) (Ownership Nothing Nothing (fromIntegral $ fileOwner status) (fromIntegral $ fileGroup status)) Nothing
 
