761b1e491b8e2601e3a49121f4bc805b09e100e4
Vanessa McHale <vamchale@gmail.com>
2019-12-02 20:42:03 -0600

    Formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -110,10 +110,10 @@ main = do
                 [ stripTime (simpleFile "a.txt" (NormalFile "text")) ]
 
 simpleFile :: FilePath -> EntryContent -> Entry
-simpleFile name what = Entry name what standardPermissions (Ownership (Just "root") (Just "root")  0 0) (Just (0,0))
+simpleFile name what = Entry name what standardPermissions (Ownership (Just "root") (Just "root") 0 0) (Just (0, 0))
 
 simpleDir :: FilePath -> Entry
-simpleDir name = Entry name Directory dirPermissions (Ownership (Just "root") (Just "root")  0 0) (Just (0,0))
+simpleDir name = Entry name Directory dirPermissions (Ownership (Just "root") (Just "root") 0 0) (Just (0, 0))
 
 dirPermissions :: Permissions
 dirPermissions = executablePermissions
