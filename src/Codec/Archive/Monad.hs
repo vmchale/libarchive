46d5d42ab78eac97b2ad74adc31e4d3b107f1828
Vanessa McHale <vamchale@gmail.com>
2020-03-15 16:20:12 -0500

    Remove bracketM for the time being

--- src/Codec/Archive/Monad.hs
+++ src/Codec/Archive/Monad.hs
@@ -5,7 +5,6 @@ module Codec.Archive.Monad ( handle
                            , withCStringArchiveM
                            , useAsCStringLenArchiveM
                            , allocaBytesArchiveM
-                           , bracketM
                            , ArchiveM
                            ) where
 
@@ -58,11 +57,3 @@ withCStringArchiveM = genBracket withCString
 
 useAsCStringLenArchiveM :: BS.ByteString -> (CStringLen -> ExceptT a IO b) -> ExceptT a IO b
 useAsCStringLenArchiveM = genBracket BS.unsafeUseAsCStringLen
-
-bracketM :: IO a -- ^ Allocate/aquire a resource
-         -> (a -> IO b) -- ^ Free/release a resource (assumed not to fail)
-         -> (a -> ArchiveM c)
-         -> ArchiveM c
-bracketM get free act =
-    flipExceptIO $
-        bracket get free (runArchiveM.act)
