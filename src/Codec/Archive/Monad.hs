2d129b47e87932016caec8a529a6eca7d3245b66
Vanessa McHale <vamchale@gmail.com>
2020-04-14 18:22:44 -0500

    Add unsafeInterleave?

--- src/Codec/Archive/Monad.hs
+++ src/Codec/Archive/Monad.hs
@@ -3,6 +3,7 @@ module Codec.Archive.Monad ( handle
                            , touchForeignPtrM
                            , runArchiveM
                            , throwArchiveM
+                           , unsafeInterleave
                            -- * Bracketed resources within 'ArchiveM'
                            , withCStringArchiveM
                            , useAsCStringLenArchiveM
@@ -13,7 +14,7 @@ module Codec.Archive.Monad ( handle
 import           Codec.Archive.Types
 import           Control.Exception      (bracket, throw)
 import           Control.Monad          (void)
-import           Control.Monad.Except   (ExceptT, runExceptT, throwError)
+import           Control.Monad.Except   (ExceptT, mapExceptT, runExceptT, throwError)
 import           Control.Monad.IO.Class
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Unsafe as BS
@@ -21,8 +22,12 @@ import           Foreign.C.String
 import           Foreign.ForeignPtr     (ForeignPtr, touchForeignPtr)
 import           Foreign.Marshal.Alloc  (allocaBytes)
 import           Foreign.Ptr            (Ptr)
+import           System.IO.Unsafe       (unsafeInterleaveIO)
 
 type ArchiveM = ExceptT ArchiveResult IO
+
+unsafeInterleave :: ArchiveM a -> ArchiveM a
+unsafeInterleave = mapExceptT unsafeInterleaveIO
 
 touchForeignPtrM :: ForeignPtr a -> ArchiveM ()
 touchForeignPtrM = liftIO . touchForeignPtr
