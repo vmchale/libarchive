d9398c9594790e085ca2b8db7e0665f227fb9f2d
Vanessa McHale <vamchale@gmail.com>
2025-11-11 13:39:43 -0500

    update gnu/savannah git url

--- libarchive-clib/update-autotools-files.sh
+++ libarchive-clib/update-autotools-files.sh
@@ -5,10 +5,10 @@ set -xe
 cd build/autoconf/
 
 rm config.guess
-curl -o config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+curl -o config.guess 'https://gitweb.git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 
 rm config.sub
-curl -o config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+curl -o config.sub 'https://gitweb.git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 
 rm install-sh
-curl -o install-sh 'https://git.savannah.gnu.org/gitweb/?p=automake.git;a=blob_plain;f=lib/install-sh;hb=HEAD'
+curl -o install-sh 'https://gitweb.git.savannah.gnu.org/gitweb/?p=automake.git;a=blob_plain;f=lib/install-sh;hb=HEAD'
